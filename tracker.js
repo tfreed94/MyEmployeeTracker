066986e7a079d890105919c3ce59d5e8ce5ce497
Troy Freed <tfreed94@gmail.com>
2021-05-17 20:15:58 -0400

    Added SQL connection and required dependencies

---
+++ tracker.js
@@ -0,0 +1,16 @@
+const cTable = require("console.table");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employeetracker"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    mainMenu();
+});
